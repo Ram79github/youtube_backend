8759d1488f34c3f19e8e24cd00bcc6ee192f4bf7
Ram krishna mishra <mishrark622@gmail.com>
2026-04-27 20:28:43 +0530

    add

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -67,7 +67,7 @@ userSchema.methods.generateAccessToken = function () {
         });
 };
 
-userSchema.methods.generateRefreshToken = async function (){
+userSchema.methods.generateRefreshToken = function (){
     return jwt.sign(
         {
             id: this._id,
